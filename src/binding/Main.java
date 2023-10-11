41eb7e15c4feb05e0e1cee1b3025075c4302c74d
imgonnabe <qorlwn456123@gmail.com>
2023-10-11 16:58:28 +0900

    lamda / binding / DI

--- src/binding/Main.java
+++ src/binding/Main.java
@@ -1,7 +1,9 @@
 package binding;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {// 실무 x
+		// static final (변하지 않음) -> 환경변수로 빼놓기도 한다.
+		
 		Super super1 = new Super();
 		Super sub = new Sub();
 
@@ -9,7 +11,7 @@ public class Main {
 		super1.printB();
 		
 		sub.printA();
-		sub.printB();
+		sub.printB();// static
 		Sub.printB();
 	}
 }
--- src/binding/Super.java
+++ src/binding/Super.java
@@ -1,6 +1,14 @@
 package binding;
 
+// @Controller
 public class Super {
+	
+	/*private final TestService testService;
+	
+	@Autowired
+	public Super(TestService testService) {// 생성자를 이용한 의존성 주입 : 생성되는 시점에 오류 확인
+		this.testService = testService;
+	}*/
 	public void printA() {
 		System.out.println("Super A");
 	}
